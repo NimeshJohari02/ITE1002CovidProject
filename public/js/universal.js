2b3e2cbb42bcfb46a89aa9e9fd493bd110a2ceaf
Nimesh Johari <nimeshjohari95@gmail.com>
2020-10-06 09:54:17 +0530

    Merged latest UI and Table

--- public/js/universal.js
+++ public/js/universal.js
@@ -1,10 +1,3 @@
-// fetch("http://api.covid19api.com/summary")
-// .then((res) => res.json())
-// .then((apidata) => {
-//   res.render("home", {
-//     deaths: apidata.Global.TotalDeaths,
-//     confirmed: apidata.Global.TotalConfirmed,
-//     recover: apidata.Global.TotalRecovered,
 const fetch = require("node-fetch");
 
 async function getData() {
